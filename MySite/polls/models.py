2fa75bf33b099d8960cf2c29ccf00483c4e5b9db
John Austin <jam31313@gmail.com>
2017-08-15 15:49:01 -0500

    Adding Django practice

--- MySite/polls/models.py
+++ MySite/polls/models.py
@@ -4,8 +4,7 @@ from django.db import models
 from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 
-
-@python_2_unicode_compatible
+@python_2_unicode_compatible  # only if you need to support Python 2
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -17,12 +16,7 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
-
-
-@python_2_unicode_compatible
+@python_2_unicode_compatible  # only if you need to support Python 2
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -30,5 +24,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
-
